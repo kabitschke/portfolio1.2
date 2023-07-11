3b8af0153027f26019d08c753e630a88aa066870
Maycon Kabitschke <mayconkabitschke@gmail.com>
2023-07-10 21:56:05 -0300

    ajustes espaçamento rodape

--- assets/js/menuMobile.js
+++ assets/js/menuMobile.js
@@ -1,13 +1,13 @@
 function monitorarLarguraDaTela() {
   // Obtém a largura atual da tela
   let largura = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-if(largura < 500){
-  document.querySelector('.header').style = "display:none";
-  document.querySelector('.header_mobile').style = "display:flex";
-}else{
-  document.querySelector('.header').style = "display:flex";
-  document.querySelector('.header_mobile').style = "display:none";
-}
+// if(largura < 500){
+//   document.querySelector('.header').style = "display:none";
+//   document.querySelector('.header_mobile').style = "display:flex";
+// }else{
+//   document.querySelector('.header').style = "display:flex";
+//   document.querySelector('.header_mobile').style = "display:none";
+// }
   // Faça algo com a largura da tela
   console.log('Largura da tela:', largura);
 }
